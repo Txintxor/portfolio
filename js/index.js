bf5ea0d69852ae4b8b0846fc87c91d772bc1e0ff
AlexQ <78956966+Txintxor@users.noreply.github.com>
2023-01-20 15:33:50 +0100

    Event listener in modal container

--- js/index.js
+++ js/index.js
@@ -1,18 +1,28 @@
+// Constantes de los elementos del dom
 
-  const modalCv = document.querySelector("#modal-cv");
-  const modalSkills = document.querySelector("#modal-skills");
-  const modalCvButton = document.querySelector("#modal-button-cv");
-  const modalSkillsButton = document.querySelector("#modal-button-skills");
-  const main = document.querySelector('.main-container');
-  
-  
-  modalCvButton.addEventListener("click", () => {
-    main.style.opacity = 0.5;
-    modalCv.style.display = "block";
-  });
+const modalCv = document.querySelector("#modal-cv");
+const modalSkills = document.querySelector("#modal-skills");
+const modalCvButton = document.querySelector("#modal-button-cv");
+const modalSkillsButton = document.querySelector("#modal-button-skills");
+const main = document.querySelector(".main-container");
+const modal = document.querySelector(".modal");
 
-  modalSkillsButton.addEventListener(
-    "click",
-    () => (modalSkills.style.display = "block")
-  );
+modalCvButton.addEventListener("click", () => {
+  main.style.opacity = 0.5;
+  modalCv.style.display = "block";
+  modal.style.display = "block";
+});
 
+modalSkillsButton.addEventListener(
+  "click",
+  () => (modalSkills.style.display = "block")
+);
+
+modal.addEventListener("touchstart", () => {
+  if (
+    modalCv.style.display === "block" ||
+    modalSkills.style.display === "block"
+  ) {
+    modal.style.display = "none";
+  }
+});
