a2025b2be929ba7f752faf4fa3c0cd65e8942eda
AlexQ <78956966+Txintxor@users.noreply.github.com>
2023-01-21 13:37:01 +0100

    Open and close modals ready

--- js/index.js
+++ js/index.js
@@ -9,20 +9,23 @@ const modal = document.querySelector(".modal");
 
 modalCvButton.addEventListener("click", () => {
   main.style.opacity = 0.5;
+  main.style.zindex = 0;
   modalCv.style.display = "block";
-  modal.style.display = "block";
 });
 
-modalSkillsButton.addEventListener(
-  "click",
-  () => (modalSkills.style.display = "block")
-);
-
-modal.addEventListener("touchstart", () => {
-  if (
-    modalCv.style.display === "block" ||
-    modalSkills.style.display === "block"
-  ) {
-    modal.style.display = "none";
-  }
+modalSkillsButton.addEventListener("click", () => {
+  main.style.opacity = 0.5;
+  main.style.zindex = 0;
+  modalSkills.style.display = "block";
 });
+
+modalCv.onclick = () => {
+  main.style.opacity = 1;
+  modalCv.style.display = "none";
+};
+
+modalSkills.onclick = () => {
+  main.style.opacity = 1;
+  modalSkills.style.display = "none";
+  console.log("hola");
+};
