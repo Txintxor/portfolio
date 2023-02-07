bde1585c44645b3e98563097d0682a801781015c
AlexQ <78956966+Txintxor@users.noreply.github.com>
2023-02-07 14:37:59 +0100

    Completed layout and functioning of website & responsive web

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,3 +1,7 @@
+//Menu responsive 768px
+//Sidebar container
+const container = document.querySelector(".side-menu-container");
+
 // Sidebar links
 const linksMenu = document.querySelector("#links-menu-container");
 const linksButton = document.querySelector("#links-li");
@@ -5,23 +9,73 @@ const linksButton = document.querySelector("#links-li");
 // Sidebar contact
 const contactMenu = document.querySelector("#contact-menu-container");
 const contactButton = document.querySelector("#contact-li");
+const width = window.matchMedia("(max-width: 768px");
 
+//Event listeners
 
-const container = document.querySelector(".pop-li");
-
-// Para hacer aparecer los menus de enlaces y contacto
+//Activa y desactiva el menu desplegable de los enlaces
 linksButton.addEventListener("mouseover", () => {
-  linksMenu.style.display = "block";
+  if (width.matches) {
+    linksMenu.style.display = "block";
+    contactButton.style.transform = "translateY(95%)";
+  }
 });
 
-contactButton.addEventListener("mouseover", () => {
-  contactMenu.style.display = "block";
+linksButton.addEventListener("mouseout", () => {
+  if (width.matches) {
+    linksMenu.style.display = "none";
+    contactButton.style.transform = "translateY(0)";
+  }
 });
+
+//Activa y desactiva el menu desplegable de contacto
+contactButton.addEventListener("mouseover", () => {
+  if (width.matches) {
+    contactMenu.style.display = "block";
+  }
+});
+
+contactButton.addEventListener("mouseout", () => {
+  if (width.matches) {
+    contactMenu.style.display = "none";
+  }
+});
+
+// Botones de activación y desactivacion de menus sin transición
+
+// const on = document.querySelector(".open-responsive-menu-button");
+// let check = false;
+
+// on.addEventListener("click", () => {
+//   if ((!check)) {
+//     // container.style.z-index = "3";
+//     container.style.transform = "translateY(0)";
+//     container.style.opacity = "1";
+//     check = true;
+//   } else {
+//     // container.style.display = "none";
+//     container.style.transform = "translateY(-110%)";
+//     container.style.opacity = "0";
+//     check = false;
+//   }
+// });
+
+// Para hacer aparecer los menus de enlaces y contacto sin transición
+
+// linksButton.addEventListener("mouseover", () => {
+
+//   linksMenu.style.display = "block";
+// });
+
+// contactButton.addEventListener("mouseover", () => {
+//   contactMenu.style.display = "block";
+// });
 
 //Para hacer desaparecer los menus
-linksButton.addEventListener("mouseout", () => {
-  linksMenu.style.display = "none";
-});
-contactButton.addEventListener("mouseout", () => {
-  contactMenu.style.display = "none";
-});
+
+// linksButton.addEventListener("mouseout", () => {
+//   linksMenu.style.display = "none";
+// });
+// contactButton.addEventListener("mouseout", () => {
+//   contactMenu.style.display = "none";
+// });
